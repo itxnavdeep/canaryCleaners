65b3d3b89e723d8f1fb933d62a3009734c3b5908
“dibyanshu11” <dibyanshu.ranjans@gmail.com>
2022-10-18 16:17:10 +0530

    new

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CustomerController extends Controller
+class CustomerController extends Controller 
 {
    
 }
